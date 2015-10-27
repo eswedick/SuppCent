2a5fe6f6b7b46141cb53271c02df6f61ddf1305d
Evan <eswedick@gmail.com>
2015-10-27 14:58:14 -0400

    script change to pull open items

--- SupportCenter/clsClientIssues.cs
+++ SupportCenter/clsClientIssues.cs
@@ -20,8 +20,11 @@ namespace SupportCenter
             strSQL = "SELECT CI.fkClientCode, CI.IssuePriority, CI.IssueSubject, CI.IssueDescription, CI.IssueOpenDate, CI.ProgressNotes " +
                         "FROM ClientIssue CI INNER JOIN Client C ON CI.fkClientCode = C.ClientCode " +
                         "WHERE C.Active = 1 " +
-                            "AND CI.fkAssignedToDeveloper = '" + pstrDeveloperCode + "'";
-
+                            "AND (CI.fkAssignedToDeveloper = '" + pstrDeveloperCode + "' " +
+                                        "OR C.fkPrimaryContactDeveloper = '" + pstrDeveloperCode + "' " +
+                                        "OR C.fkTechnicalContact = '" + pstrDeveloperCode + "') " +
+                                "AND CI.CompletedDate IS NULL " +
+                        "ORDER BY CI.IssuePriority ASC";
             dtOpenItems = Database.Query(strSQL);
 
             return dtOpenItems;
